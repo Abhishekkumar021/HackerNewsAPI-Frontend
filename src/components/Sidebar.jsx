69387874fe69cb8c0ffe79bdedf3fb668d5f650e
Abhishek Kumar <abhishekkumar232105@gmail.com>
2024-09-28 11:10:13 +0530

    Now it is available for mobile devices

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,82 @@
-import React from 'react';
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import '../styles/sidebar.css';
+
+// const Sidebar = () => {
+//     const [isOpen, setIsOpen] = useState(false);
+
+//     const handleMenuToggle = (e) => {
+//         setIsOpen(!isOpen);
+//     }
+
+
+//     return (
+//         <div className={`sidebar ${isOpen ? 'show' : ''}`}>
+//             <div className={`logo ${isOpen ? 'burgerLogo' : ''}`}>
+//                 <Link to="/">Daily News</Link>
+//             </div>
+//             <div className={`sidebar-links-main`}>
+//                 <div className="sidebar-links">
+//                     <Link to="/top-new-stories" onClick={handleMenuToggle}>New Stories</Link>
+//                     <Link to="/top-stories" onClick={handleMenuToggle}>Top Stories</Link>
+//                     <Link to="/top-best-stories" onClick={handleMenuToggle}>Best Stories</Link>
+//                     <Link to="/show-stories" onClick={handleMenuToggle}>Show Stories</Link>
+//                     <Link to="/job-stories" onClick={handleMenuToggle}>Job Stories</Link>
+//                 </div>
+//                 <div className={`hamburgerBtn ${isOpen ? 'show' : ''}`} onClick={handleMenuToggle}>
+//                     <span className='bar1 bar'></span>
+//                     <span className='bar2 bar'></span>
+//                     <span className='bar3 bar'></span>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Sidebar;
+
+
+
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/sidebar.css';
 
 const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleMenuToggle = (e) => {
+        setIsOpen(!isOpen);
+    }
+
+    const handleLinkClick = () => {
+        setIsOpen(false);
+    }
+
     return (
-        <div className="sidebar">
-            <div className='logo'>
+        <div className={`sidebar-main-div ${isOpen ? 'show' : ''}`}>
+            <div className={`hamburgerBtn ${isOpen ? 'show' : ''}`} onClick={handleMenuToggle}>
+                <span className='bar1 bar'></span>
+                <span className='bar2 bar'></span>
+                <span className='bar3 bar'></span>
+            </div>
+
+            <div className={`logo`}>
                 <Link to="/">Daily News</Link>
             </div>
-            <div className="sidebar-links-main">
-                <div className="sidebar-links">
-                    {/* <Link to="/">Home</Link> */}
-                    <Link to="/top-new-stories">New Stories</Link>
-                    <Link to="/top-stories">Top Stories</Link>
-                    <Link to="/top-best-stories">Best Stories</Link>
-                    <Link to="/show-stories">Show Stories</Link>
-                    <Link to="/job-stories">Job Stories</Link>
+            {/* <div className={`logo ${isOpen ? 'burgerLogo' : ''}`}>
+                <Link to="/">Daily News</Link>
+            </div> */}
+            <div className={`sidebar ${isOpen ? 'show' : ''}`}>
+
+                <div className={`sidebar-links-main`}>
+                    <div className="sidebar-links">
+                        <Link to="/top-new-stories" onClick={handleLinkClick}>New Stories</Link>
+                        <Link to="/top-stories" onClick={handleLinkClick}>Top Stories</Link>
+                        <Link to="/top-best-stories" onClick={handleLinkClick}>Best Stories</Link>
+                        <Link to="/show-stories" onClick={handleLinkClick}>Show Stories</Link>
+                        <Link to="/job-stories" onClick={handleLinkClick}>Job Stories</Link>
+                    </div>
                 </div>
             </div>
         </div>
